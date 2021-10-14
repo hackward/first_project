3ff4d8f0f48dfa092d8ebdaf1cf6e862839add06
hackward <hackward@users.noreply.github.com>
2021-10-14 16:01:51 +0000

    ✅ Ready to clone and code.

--- first_project/base.py
+++ first_project/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+first_project base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the first_project project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from first_project import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
